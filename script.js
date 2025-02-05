07d930a916ef8f0af667ec65af1765993372048a
kgsmithcs <130272579+kgsmithcs@users.noreply.github.com>
2025-02-04 19:29:56 -0500

    Created Homepage layout

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function() {
+        alert("Get a quote for your cleaning today!"); //Temporary - will replace with a slide-in form
+    }, 4000);
+});
